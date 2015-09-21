33af8b1a07ec5bd0adbf15c64aa47c8782fb1c8a
Michael Baggott <michaelbaggott@hotmail.com>
2015-09-21 17:04:28 +1000

    Updated to get CAS redirection working

--- custom_study_plan_generator/App_Start/Startup.Auth.cs
+++ custom_study_plan_generator/App_Start/Startup.Auth.cs
@@ -67,8 +67,7 @@ namespace custom_study_plan_generator
 
             CasAuthenticationOptions casOptions = new CasAuthenticationOptions()
             {
-                CasServerUrlBase = "https://ec2-52-88-235-117.us-west-2.compute.amazonaws.com:8443/cas-server-webapp-4.0.0",
-				CallbackPath = new PathString("/WEB-INF/view/jsp/default/ui/casConfirmView.jsp")
+                CasServerUrlBase = "https://ec2-52-88-235-117.us-west-2.compute.amazonaws.com:8443/cas-server-webapp-4.0.0"
             };
             app.UseCasAuthentication(casOptions);
         }
