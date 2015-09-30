4d6daac4c8a12e02546eac00448fa345369a23b4
Harrison Slater <s3439870@student.rmit.edu.au>
2015-09-30 15:42:56 +1000

    Possible fix for CAS login sometimes not working
    Issue is detailed here: https://stackoverflow.com/questions/20737578/asp-net-sessionid-owin-cookies-do-not-send-to-browser/21234614#21234614

--- custom_study_plan_generator/Controllers/AccountController.cs
+++ custom_study_plan_generator/Controllers/AccountController.cs
@@ -281,6 +281,7 @@ namespace custom_study_plan_generator.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult ExternalLogin(string provider, string returnUrl)
         {
+            Session["dummy"] = "dummy";
             // Request a redirect to the external login provider
             return new ChallengeResult(provider, Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl }));
         }
