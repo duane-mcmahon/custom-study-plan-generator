468c464ab7f0ebed07e5e308037e75f553041345
Michael Baggott <michaelbaggott@hotmail.com>
2015-11-03 16:06:37 +1000

    Students CRUD complete

---
+++ custom_study_plan_generator/Controllers/StudentsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using custom_study_plan_generator.Models;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Data.Entity.Infrastructure;
+
+namespace custom_study_plan_generator.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private custom_study_plan_generatorEntities db = new custom_study_plan_generatorEntities();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "student_id,firstname,lastname")] Student student)
+        {
+
+            var studentCheck = from stud in db.Students
+                               where stud.student_id == student.student_id
+                               select stud;
+
+            if (studentCheck.Count() > 0)
+            {
+                Session["studentExists"] = "true";
+                return View(student);
+            }
+
+
+            
+            
+            
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "student_id,firstname,lastname")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException.ToString().Contains("The DELETE statement conflicted with the REFERENCE constraint"))
+                {
+                    Session["ForeignKeyConstraint"] = "true";
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ custom_study_plan_generator/Scripts/StudentCreate.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+
+    $('#studentExists').delay(3000).fadeOut(2000);
+
+});
