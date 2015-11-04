88e7c6dd23709bcb30504dc955c67dcb51778904
Michael Baggott <michaelbaggott@hotmail.com>
2015-11-04 14:21:41 +1000

    Set up data validation for adding a Student. Created meta classes ready to go for validation on Course and Unit CRUD once database requirements have been finalised

---
+++ custom_study_plan_generator/MetaObjects/CourseMeta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace custom_study_plan_generator.MetaObjects
+{
+    public class CourseMeta
+    {
+        [NotMapped]
+        [Required]
+        public string course_code { get; set; }
+
+        [NotMapped]
+        [Required]
+        public string name { get; set; }
+        
+        [NotMapped]
+        [Required]
+        [Range(0,64)]
+        public int num_units { get; set; }
+
+        /* Check if wee need this property */
+        public int duration { get; set; }
+
+        /* Check if we need this property */
+        public int max_credit { get; set; }
+
+    }
+}
---
+++ custom_study_plan_generator/MetaObjects/UnitMeta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace custom_study_plan_generator.MetaObjects
+{
+    public class UnitMeta
+    {
+
+        [NotMapped]
+        [Required]
+        public string unit_code { get; set; }
+
+        [NotMapped]
+        [Required]
+        public string name { get; set; }
+
+        /* Check if we need this property */
+        public string type_code { get; set; }
+
+        /* Will need client side validation to make sure on or the other is set */
+        [NotMapped]
+        public bool semester1 { get; set; }
+
+        [NotMapped]
+        public bool semester2 { get; set; }
+
+        /* Need to check if this is required */
+        [Range(1, 8)]
+        public Nullable<int> preferred_year { get; set; }
+
+    }
+}
